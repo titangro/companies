7c490fe3cab0fb6c6f5ef17b79d5596cebf15f24
titangro <mustbe09@yandex.ru>
2019-08-12 11:35:50 +0300

    Updates

---
+++ precache-manifest.8bb4a0a0746eda82cf2f0e0ba0e4cf3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0827a3969eb057cfbe283375d51cc624",
+    "url": "/companies/index.html"
+  },
+  {
+    "revision": "b0d3d2badb3cc221dda2",
+    "url": "/companies/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4ef16570b8ed0fb12ca6",
+    "url": "/companies/static/css/main.4488af55.chunk.css"
+  },
+  {
+    "revision": "b0d3d2badb3cc221dda2",
+    "url": "/companies/static/js/2.f4d5f43b.chunk.js"
+  },
+  {
+    "revision": "4ef16570b8ed0fb12ca6",
+    "url": "/companies/static/js/main.254528e2.chunk.js"
+  },
+  {
+    "revision": "a40f428f1535cdb4f6f8",
+    "url": "/companies/static/js/runtime~main.e0adf6b6.js"
+  }
+]);
